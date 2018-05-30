bbe65f0567f2ba988e717222977748d94b91b9c0
Geoffrey L. Angus <glangus@us.ibm.com>
2018-05-29 23:14:41 -0700

    more spaces

--- differentiator/reader.py
+++ differentiator/reader.py
@@ -4,12 +4,12 @@ import re
 class TextReader(object):
 
         def __init__(self, text_path, regex_rules):
-        	self.text_path = text_path
-		self.file = open(text_path, "r")
-		self.regex_rules = regex_rules
+            self.text_path = text_path
+        self.file = open(text_path, "r")
+        self.regex_rules = regex_rules
 
-	# Source: https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
-	def read_word():
+    # Source: https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
+    def read_word():
         while True:
             buf = self.file.read(10240)
             if not buf:
@@ -28,19 +28,19 @@ class TextReader(object):
         yield '' #handle the scene that the file is empty
 
 
-	def get_next_sentence():
-		sentence = []
-		for word in self.read_word():
-			sentence.append(word)
-			if word == '.':
-				return self.clean_sentence(sentence)
-		return None
+    def get_next_sentence():
+        sentence = []
+        for word in self.read_word():
+            sentence.append(word)
+            if word == '.':
+                return self.clean_sentence(sentence)
+        return None
 
 
-	def clean_sentence(sentence_arr):
-		sentence = ' '.join(sentence_arr)
-		new_text = re.sub(self.regex_rules, '', new_text)
-		return new_text.split(' ')
+    def clean_sentence(sentence_arr):
+        sentence = ' '.join(sentence_arr)
+        new_text = re.sub(self.regex_rules, '', new_text)
+        return new_text.split(' ')
 
-	def __del__(self):
-		self.file.close()
+    def __del__(self):
+        self.file.close()

d5dbc84d00e77c360b15dfce7a7d9c1cae7a0eb8
Geoffrey L. Angus <glangus@us.ibm.com>
2018-05-30 07:38:46 -0700

    my own generator??

--- differentiator/reader.py
+++ differentiator/reader.py
@@ -8,25 +8,6 @@ class TextReader(object):
         self.file = open(text_path, "r")
         self.regex_rules = regex_rules
 
-    # Source: https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
-    def read_word(self):
-        while True:
-            buf = self.file.read(10240)
-            if not buf:
-                break
-
-            # make sure we end on a space (word boundary)
-            while not str.isspace(buf[-1]):
-                ch = self.file.read(1)
-                if not ch:
-                    break
-                buf += ch
-
-            words = buf.split()
-            for word in words:
-                yield word
-        yield '' #handle the scene that the file is empty
-
     # Expand this as necessary.
     def preprocess_text(self, outfile, rules):
         with open(outfile, 'w') as f:
@@ -38,12 +19,18 @@ class TextReader(object):
         self.file.seek(0)
 
     def get_next_sentence(self):
-        sentence = []
-        for word in self.read_word():
-            sentence.append(word)
-            if word == '.':
-                print(' '.join(sentence))
-                yield self.clean_sentence(sentence)
+        while True:
+            buf = self.file.read(1)
+            if not buf:
+                break
+            while buf[-2:] != '. ':
+                ch = self.file.read(1)
+                if not ch:
+                    break
+                buf += ch
+
+            sentence = buf.split()
+            yield self.clean_sentence(sentence)
         yield None
 
     def clean_sentence(self, sentence_arr):

12f93f5dc7dcb83ac22a691f6ffa71d885da67e4
Pierce Freeman <piercefreeman@users.noreply.github.com>
2018-05-29 23:32:51 -0700

    added testing harness

---
+++ testor/predict.py
@@ -0,0 +1,23 @@
+from scws import SCWSHarness
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+harness = SCWSHarness()
+
+# Generate the X/y pairs of our dataset
+X = [
+    (
+        ex.word1,
+        harness.get_context(ex, word="word1"),
+        ex.word2,
+        harness.get_context(ex, word="word2")
+    ) for ex in harness.dataset]
+y = [int(float(ex.avg_rating) > 5) for ex in harness.dataset]
+
+# Split our dataset
+# We want to train on only a small part of our data, so we leave a high success
+# rate to evaluate on our scws task
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.80, random_state=42)
+
+for a in zip(X_train, y_train):
+    print(a)
